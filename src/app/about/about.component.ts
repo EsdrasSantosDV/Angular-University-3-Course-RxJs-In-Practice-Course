db417f61d4cbdef4a781331df2f6b41d1dbadbf4
EsdrasSantosDV <esdrassantosoliveiradeveloper@gmail.com>
2022-11-16 06:52:28 -0300

    feat:Observable conceito

--- src/app/about/about.component.ts
+++ src/app/about/about.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit, ViewEncapsulation } from '@angular/core';
-import {interval} from 'rxjs';
+import {fromEvent, interval, timer} from 'rxjs';
 
 @Component({
   selector: 'about',
@@ -11,11 +11,16 @@ export class AboutComponent implements OnInit {
   constructor() { }
 
   ngOnInit() {
-    const interval$=interval(1000);
+    const interval$=timer(3000,1000);
 
+    interval$.subscribe(val=>console.log("stream  1 => "+val));
 
+    interval$.subscribe(val=>console.log("stream 2 =>"+val));
 
+    const click$=fromEvent(document,'click');
 
+    //QUEREMOS O VLAOR DO FLUXO
+    click$.subscribe(evt=>console.log(evt));
 
 
   }
