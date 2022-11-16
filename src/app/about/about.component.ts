58c77110b1c11b8e14a9dd674b8fbf03c0de08cc
EsdrasSantosDV <esdrassantosoliveiradeveloper@gmail.com>
2022-11-16 07:44:36 -0300

    feat:create um observer http

--- src/app/about/about.component.ts
+++ src/app/about/about.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit, ViewEncapsulation } from '@angular/core';
-import {fromEvent, interval, timer} from 'rxjs';
+import {fromEvent, interval, noop, Observable, timer} from 'rxjs';
 
 @Component({
   selector: 'about',
@@ -11,16 +11,24 @@ export class AboutComponent implements OnInit {
   constructor() { }
 
   ngOnInit() {
-    const interval$=timer(3000,1000);
+    const http$=Observable.create(observer=>{
+      fetch('http://localhost:9000/api/courses').then(response=>{
+        return response.json();
+      })
+        .then(body=>{
+          observer.next(body);
+          observer.complete();
+        })
+        .catch(err=>{
+          observer.error(err);
+        })
+    });
 
-    interval$.subscribe(val=>console.log("stream  1 => "+val));
-
-    interval$.subscribe(val=>console.log("stream 2 =>"+val));
-
-    const click$=fromEvent(document,'click');
-
-    //QUEREMOS O VLAOR DO FLUXO
-    click$.subscribe(evt=>console.log(evt));
+    http$.subscribe(courses =>
+      console.log(courses),
+      noop,
+      ()=>console.log('complete')
+    );
 
 
   }
