2187649391dc9290b0e80e6f20709b02d3edd62d
EsdrasSantosDV <esdrassantosoliveiradeveloper@gmail.com>
2022-11-21 08:26:22 -0300

    feat:concat operator

--- src/app/about/about.component.ts
+++ src/app/about/about.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit, ViewEncapsulation } from '@angular/core';
-import {fromEvent, interval, noop, Observable, timer} from 'rxjs';
+import {concat, fromEvent, interval, noop, Observable, of, timer} from 'rxjs';
 import {createHttpObservable} from '../common/util';
 import {map} from 'rxjs/operators';
 
@@ -14,13 +14,22 @@ export class AboutComponent implements OnInit {
 
   ngOnInit() {
 
+    //NESSE CASO DO INTERVALL NUNCA VAMOS VER O FLUXO 2 E 32
+    const source1$=interval(1000);
+
+    const source2$=of(4,5,6);
+
+    const source3$=of(7,8,9);
+
+    const result$=concat(
+      source1$,
+      source2$,
+      source3$,
+    )
+
+    result$.subscribe(console.log);
 
 
-    const numbers = interval(1000);
-
-    const vezes100$=numbers.pipe(map(res=>res*100));
-
-    vezes100$.subscribe((res)=>console.log(res));
 
 
 
