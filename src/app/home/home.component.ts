b20c318f947123b06e23ba0b13e9f3302537ed3b
EsdrasSantosDV <esdrassantosoliveiradeveloper@gmail.com>
2022-11-21 08:13:11 -0300

    feat:shared replay

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -18,11 +18,23 @@ export class HomeComponent implements OnInit {
     ngOnInit() {
       const http$:Observable<Course> = createHttpObservable('http://localhost:9000/api/courses');
       //pipe operator
+
+      //PRECIAMOS CRIAR UMA SUBSCRIPTION
       const courses$ = http$.pipe(
+        tap(()=>console.log("HTTP REQUEST EXECUTED")),
         map(
-          res=> Object.values<Course>(res["payload"])
-        )
+            res=> Object.values<Course>(res["payload"]),
+        ),
+        //VOCE UTILIZA ISSO PARA COMPARTILHAR PARA VARIAS ASSINATURAS
+        //FAZENDO COM QUE POR EXEMPLO, NÃO FAÇA VARIAS REQUISIÇOES POR EXEMPLO
+        shareReplay(),
       );
+
+      //NESSE CASO AS DUAS ESTÃO PEGANDO DO MESMO HTTP
+      //FAZENDO DUAS REQUISIÇOES DISTINTAS
+      //E NÃO QUEREMOS ISSO
+       courses$.subscribe();
+
 
       this.beginnerCourses$=courses$
         .pipe(
