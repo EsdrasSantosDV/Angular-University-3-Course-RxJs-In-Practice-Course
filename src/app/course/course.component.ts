55ad66208360ec19168f11daa8012dfdb3be4707
EsdrasSantosDV <esdrassantosoliveiradeveloper@gmail.com>
2022-11-21 09:43:57 -0300

    feat:observable http request lessons

--- src/app/course/course.component.ts
+++ src/app/course/course.component.ts
@@ -15,6 +15,7 @@ import {
 } from 'rxjs/operators';
 import {merge, fromEvent, Observable, concat} from 'rxjs';
 import {Lesson} from '../model/lesson';
+import {createHttpObservable} from '../common/util';
 
 
 @Component({
@@ -29,21 +30,18 @@ export class CourseComponent implements OnInit, AfterViewInit {
     lessons$: Observable<Lesson[]>;
 
 
+
     @ViewChild('searchInput', { static: true }) input: ElementRef;
 
     constructor(private route: ActivatedRoute) {
-
-
     }
 
     ngOnInit() {
-
-        const courseId = this.route.snapshot.params['id'];
-
-
+      const courseId = this.route.snapshot.params['id'];
+      this.course$ = createHttpObservable(`http://localhost:9000/api/courses/${courseId}`);
+      this.lessons$ = createHttpObservable(`http://localhost:9000/api/lessons?courseId=${courseId}&pageSize=100`).pipe(map(res=> res['payload']));
 
     }
-
     ngAfterViewInit() {
 
 
