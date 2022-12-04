a9b94309a11481f34754e5839101073e6157f1ef
EsdrasSantosDV <esdrassantosoliveiradeveloper@gmail.com>
2022-12-04 16:54:32 -0300

    feat:rxjs operator startWith

--- src/app/course/course.component.ts
+++ src/app/course/course.component.ts
@@ -49,8 +49,10 @@ export class CourseComponent implements OnInit, AfterViewInit {
 
       //VAMOS CRIAR UM EVENTO
       //TAMOS CRIANDO UM FLUXO LIGANDO O INPUT DO CAMPO DE PESQUISA, AO EVENTO DE KEYUP
-      const searchLessons$=fromEvent<any>(this.input.nativeElement,'keyup').pipe(
+      this.lessons$=fromEvent<any>(this.input.nativeElement,'keyup').pipe(
         map(event=>event.target.value),
+        //START WITH E O VALOR INCIIAL DO NOSSO FLUXO
+        startWith(''),
         //Debounce time descarta os valores emitidos que levam menos que o tempo especificado
         //entre a saida
         //atrasa os valores emitidos por uma fonte para o devido tempo dado.
@@ -63,12 +65,7 @@ export class CourseComponent implements OnInit, AfterViewInit {
         switchMap(search=> this.loadLessons(search))
         );
 
-      const initialLessons$=this.loadLessons();
 
-
-
-
-      this.lessons$ =concat(initialLessons$, searchLessons$);
 
     }
 
