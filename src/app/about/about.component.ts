80a7d0862ebbffe6422f2d0815f8d515a0114558
EsdrasSantosDV <esdrassantosoliveiradeveloper@gmail.com>
2022-12-07 08:49:17 -0300

    feat:Subjects explanation

--- src/app/about/about.component.ts
+++ src/app/about/about.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit, ViewEncapsulation } from '@angular/core';
-import {concat, fromEvent, interval, merge, noop, Observable, of, timer} from 'rxjs';
+import {concat, fromEvent, interval, merge, noop, Observable, of, Subject, timer} from 'rxjs';
 import {createHttpObservable} from '../common/util';
 import {map} from 'rxjs/operators';
 import {Course} from '../model/course';
@@ -15,51 +15,51 @@ export class AboutComponent implements OnInit {
 
   ngOnInit() {
 
-    //NESSE CASO DO INTERVALL NUNCA VAMOS VER O FLUXO 2 E 32
-    const source1$=interval(1000);
+    //O que é um Sujeito? Um Subject RxJS é um tipo especial de Observable
+    // que permite que os valores sejam multicast para muitos Observers.
+    const subject =new Subject();
 
-    const source2$=of(4,5,6);
+    subject.subscribe(console.log);
 
-    const source3$=of(7,8,9);
+    subject.subscribe(console.log);
 
-    const result$=concat(
-      source1$,
-      source2$,
-      source3$,
-    )
-
-   // result$.subscribe(console.log);
+    subject.next(1);
+    subject.next(1);
+    subject.next(2);
+    subject.next(1);
 
 
-
-    const interval1$=interval(1000);
-    const interval3$=interval(10000);
-    const insterval2$=interval1$.pipe(map(val=>10*val));
-
-    //TRANSFORMA VARIOS OBSERVABLES EM UM SO
-    const result2$=merge(interval1$,insterval2$,interval3$);
-
-   // result2$.subscribe(console.log)
-
-
-    const int1$= interval(1000);
-
-    const sub= int1$.subscribe();
-
-
-
-    setTimeout(()=>sub.unsubscribe(),5000);
-
-    const http$:Observable<Course> = createHttpObservable('http://localhost:9000/api/courses');
-
-
-    http$.subscribe(console.log);
-    const sub1= http$.subscribe();
-    setTimeout(()=>sub1.unsubscribe(),0);
-
-
-
-
+   //  //NESSE CASO DO INTERVALL NUNCA VAMOS VER O FLUXO 2 E 32
+   //  const source1$=interval(1000);
+   //
+   //  const source2$=of(4,5,6);
+   //
+   //  const source3$=of(7,8,9);
+   //
+   //  const result$=concat(
+   //    source1$,
+   //    source2$,
+   //    source3$,
+   //  )
+   // // result$.subscribe(console.log);
+   //  const interval1$=interval(1000);
+   //  const interval3$=interval(10000);
+   //  const insterval2$=interval1$.pipe(map(val=>10*val));
+   //  //TRANSFORMA VARIOS OBSERVABLES EM UM SO
+   //  const result2$=merge(interval1$,insterval2$,interval3$);
+   // // result2$.subscribe(console.log)
+   //
+   //  const int1$= interval(1000);
+   //
+   //  const sub= int1$.subscribe();
+   //
+   //  setTimeout(()=>sub.unsubscribe(),5000);
+   //
+   //  const http$:Observable<Course> = createHttpObservable('http://localhost:9000/api/courses');
+   //
+   //  http$.subscribe(console.log);
+   //  const sub1= http$.subscribe();
+   //  setTimeout(()=>sub1.unsubscribe(),0);
 
   }
 
